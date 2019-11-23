0f4a7cc91deccb548248b034a56103e67915f2de
MZainKh <mz46@hw.ac.uk>
2019-11-23 15:06:20 +0400

    updates

--- idk.js
+++ idk.js
@@ -11,31 +11,43 @@ serv.listen(2060)
 console.log('Server started')
 
 var SOCKET_LIST = {}
+var PLAYER_LIST = {}
+
+var Player = function (id) {
+  var self = {
+    x: 250,
+    y: 250,
+    id: id,
+    number: '' + Math.floor(10 * Math.random())
+  }
+  return self
+}
 
 var io = require('socket.io')(serv, {})
 io.sockets.on('connection', function (socket) {
   socket.id = Math.random()
-  socket.x = 0
-  socket.y = 0
-  socket.number = '' + Math.floor(10 * Math.random())
   SOCKET_LIST[socket.id] = socket
   console.log('socket connection')
 
+  var player = Player(socket.id)
+  PLAYER_LIST[socket.id] = player
+
   socket.on('disconnect', function () {
     delete SOCKET_LIST[socket.id]
+    delete PLAYER_LIST[socket.id]
   })
 })
 
 setInterval(function () {
   var pack = []
-  for (var i in SOCKET_LIST) {
-    var socket = SOCKET_LIST[i]
-    socket.x++
-    socket.y++
+  for (var i in PLAYER_LIST) {
+    var player = PLAYER_LIST[i]
+    player.x++
+    player.y++
     pack.push({
-      x: socket.x,
-      y: socket.y,
-      number: socket.number
+      x: player.x,
+      y: player.y,
+      number: player.number
     })
   }
   for (var i in SOCKET_LIST) {
